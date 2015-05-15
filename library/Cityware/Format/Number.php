1118b9d911ad846346d80b6f6ae42a10b9eaf788
Fabricio Valadares <valadares.fabricio@gmail.com>
2015-05-15 17:40:39 -0300

    Update
    Update

---
+++ library/Cityware/Format/Number.php
@@ -0,0 +1,56 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace Cityware\Format;
+
+/**
+ * Description of Number
+ *
+ * @author fabricio.xavier
+ */
+class Number {
+
+    /**
+     * Formatação e numero inteiro
+     * @param float/integer $value
+     * @param string $language
+     * @return integer
+     */
+    public static function integerNumber($value, $language = 'pt_BR') {
+        $valInteger = new \NumberFormatter($language, \NumberFormatter::DECIMAL);
+        $valInteger->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, 0);
+        return $valInteger->format($value, \NumberFormatter::TYPE_INT64);
+    }
+    
+    /**
+     * Formatação de numero formato moeda
+     * @param float/integer $value
+     * @param integer $precision
+     * @param string $language
+     * @return float
+     */
+    public static function currency($value, $precision = 2, $language = 'pt_BR') {
+        $valCurrency = new \NumberFormatter($language, \NumberFormatter::CURRENCY);
+        $valCurrency->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $precision);
+        return $valCurrency->format((float) $value, \NumberFormatter::TYPE_DOUBLE);
+    }
+    
+    /**
+     * Formatação de numero decimal
+     * @param float/integer $value
+     * @param integer $precision
+     * @param string $language
+     * @return float
+     */
+    public static function decimalNumber($value, $precision = 2, $language = 'pt_BR') {
+        $valCurrency = new \NumberFormatter($language, \NumberFormatter::DECIMAL);
+        $valCurrency->setAttribute(\NumberFormatter::MAX_FRACTION_DIGITS, $precision);
+        return $valCurrency->format((float) $value, \NumberFormatter::TYPE_DOUBLE);
+    }
+
+}
